7eb69fa597746fba145ac23ce6cabd7ebe74bac9
mike lorengo <mikelor@gmail.com>
2020-02-25 14:36:45 -0800

    Basic Sample

---
+++ Assets/MixedRealityToolkit.ThirdParty/OculusQuestInput/Scripts/Config/MRTKOculusConfig.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+namespace prvncher.MixedReality.Toolkit.Config
+{
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
+    [CreateAssetMenu(menuName = "MRTK-Quest/MRTK-OculusConfig")]
+    public class MRTKOculusConfig : ScriptableObject
+    {
+        private static MRTKOculusConfig instance;
+        public static MRTKOculusConfig Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Resources.Load<MRTKOculusConfig>("MRTK-OculusConfig");
+
+                    if (instance == null)
+                    {
+                        UnityEngine.Debug.LogError("Failure to detect MRTK-OculusConfig. Please create an instance using the asset context menu, and place it in any Resources folder.");
+                    }
+                }
+                return instance;
+            }
+        }
+        [Header("Config")]
+        [SerializeField]
+        [Tooltip("Using avatar hands requires a local avatar prefab. Failure to provide one will result in nothing being displayed. \n\n" +
+                 "Note: In order to render avatar hands, you will need to set an app id in OvrAvatarSettings. Any number will do, but it needs to be set.")]
+        private bool renderAvatarHandsInsteadOfControllers = true;
+
+        /// <summary>
+        /// Using avatar hands requires a local avatar prefab. Failure to provide one will result in nothing being displayed.
+        /// </summary>
+        public bool RenderAvatarHandsInsteadOfController => renderAvatarHandsInsteadOfControllers;
+
+        [Header("Prefab references")]
+        [SerializeField]
+        [Tooltip("Prefab reference for OVRCameraRig to load, if none are found in scene.")]
+        private OVRCameraRig ovrCameraRigPrefab = null;
+
+        /// <summary>
+        /// Prefab reference for OVRCameraRig to load, if none are found in scene.
+        /// </summary>
+        public OVRCameraRig OVRCameraRigPrefab => ovrCameraRigPrefab;
+
+        [SerializeField]
+        [Tooltip("Prefab reference for LocalAvatar to load, if none are found in scene.")]
+        private GameObject localAvatarPrefab = null;
+
+        /// <summary>
+        /// Prefab reference for LocalAvatar to load, if none are found in scene.
+        /// </summary>
+        public GameObject LocalAvatarPrefab => localAvatarPrefab;
+
+
+        [Header("Performance Configuration")]
+        [SerializeField]
+        [Tooltip("Default CPU performance level (0-2 is documented), (3-5 is undocumented).")]
+        [Range(0, 5)]
+        private int defaultCpuLevel = 2;
+
+        /// <summary>
+        /// Accessor for the Oculus CPU power level.
+        /// </summary>
+        public int CPULevel
+        {
+            get => defaultCpuLevel;
+            set
+            {
+                defaultCpuLevel = value;
+#if !UNITY_EDITOR
+                OVRManager.cpuLevel = defaultCpuLevel;
+#endif
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Default GPU performance level (0-2 is documented), (3-5 is undocumented).")]
+        [Range(0, 5)]
+        private int defaultGpuLevel = 2;
+
+        /// <summary>
+        /// Accessor for the Oculus GPU power level.
+        /// </summary>
+        public int GPULevel
+        {
+            get => defaultGpuLevel;
+            set
+            {
+                defaultGpuLevel = value;
+#if !UNITY_EDITOR
+                OVRManager.gpuLevel = defaultGpuLevel;
+#endif
+            }
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.ThirdParty/OculusQuestInput/Scripts/Input/OculusQuestInputManager.cs
@@ -0,0 +1,333 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using prvncher.MixedReality.Toolkit.Config;
+using UnityEngine;
+
+namespace prvncher.MixedReality.Toolkit.OculusQuestInput
+{
+    /// <summary>
+    /// Manages Oculus Quest Hand Inputs
+    /// </summary>
+    [MixedRealityDataProvider(typeof(IMixedRealityInputSystem), SupportedPlatforms.Android | SupportedPlatforms.WindowsStandalone, "Oculus Quest Input Manager")]
+    public class OculusQuestInputManager : BaseInputDeviceManager, IMixedRealityCapabilityCheck
+    {
+        private Dictionary<Handedness, OculusQuestHand> trackedHands = new Dictionary<Handedness, OculusQuestHand>();
+        private Dictionary<Handedness, OculusQuestController> trackedControllers = new Dictionary<Handedness, OculusQuestController>();
+
+        private OVRCameraRig cameraRig;
+
+        private OVRHand rightHand;
+        private OVRSkeleton rightSkeleton;
+
+        private OVRHand leftHand;
+        private OVRSkeleton leftSkeleton;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="registrar">The <see cref="IMixedRealityServiceRegistrar"/> instance that loaded the data provider.</param>
+        /// <param name="inputSystem">The <see cref="Microsoft.MixedReality.Toolkit.Input.IMixedRealityInputSystem"/> instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public OculusQuestInputManager(
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(inputSystem, name, priority, profile)
+        {
+        }
+
+        public override void Enable()
+        {
+            base.Enable();
+            SetupInput();
+            ConfigurePerformancePreferences();
+        }
+
+        private void SetupInput()
+        {
+            cameraRig = GameObject.FindObjectOfType<OVRCameraRig>();
+            if (cameraRig == null)
+            {
+                var mainCamera = Camera.main;
+                Transform cameraParent = null;
+                if (mainCamera != null)
+                {
+                    cameraParent = mainCamera.transform.parent;
+
+                    // Destroy main camera
+                    GameObject.Destroy(cameraParent.gameObject);
+                }
+
+                // Instantiate camera rig as a child of the MixedRealityPlayspace
+                cameraRig = GameObject.Instantiate(MRTKOculusConfig.Instance.OVRCameraRigPrefab);
+            }
+
+            bool useAvatarHands = MRTKOculusConfig.Instance.RenderAvatarHandsInsteadOfController;
+            // If using Avatar hands, de-activate ovr controller rendering
+            foreach (var controllerHelper in cameraRig.gameObject.GetComponentsInChildren<OVRControllerHelper>())
+            {
+                controllerHelper.gameObject.SetActive(!useAvatarHands);
+            }
+
+            if (useAvatarHands)
+            {
+                // Initialize the local avatar controller
+                GameObject.Instantiate(MRTKOculusConfig.Instance.LocalAvatarPrefab, cameraRig.trackingSpace);
+            }
+
+            var ovrHands = cameraRig.GetComponentsInChildren<OVRHand>();
+
+            foreach (var ovrHand in ovrHands)
+            {
+                var skeltonDataProvider = ovrHand as OVRSkeleton.IOVRSkeletonDataProvider;
+                var skeltonType = skeltonDataProvider.GetSkeletonType();
+
+                var ovrSkelton = ovrHand.GetComponent<OVRSkeleton>();
+                if (ovrSkelton == null)
+                {
+                    continue;
+                }
+
+                switch (skeltonType)
+                {
+                    case OVRSkeleton.SkeletonType.HandLeft:
+                        leftHand = ovrHand;
+                        leftSkeleton = ovrSkelton;
+                        break;
+                    case OVRSkeleton.SkeletonType.HandRight:
+                        rightHand = ovrHand;
+                        rightSkeleton = ovrSkelton;
+                        break;
+                }
+            }
+        }
+
+        private void ConfigurePerformancePreferences()
+        {
+#if !UNITY_EDITOR
+            OVRManager.cpuLevel = MRTKOculusConfig.Instance.CPULevel;
+           OVRManager.gpuLevel = MRTKOculusConfig.Instance.GPULevel;
+#endif
+        }
+
+        public override void Disable()
+        {
+            base.Disable();
+
+            RemoveAllControllerDevices();
+            RemoveAllHandDevices();
+        }
+
+        public override IMixedRealityController[] GetActiveControllers()
+        {
+            if (trackedHands.Values.Count > 0)
+            {
+                return trackedHands.Values.ToArray<IMixedRealityController>();
+            }
+            else if (trackedControllers.Values.Count > 0)
+            {
+                return trackedControllers.Values.ToArray<IMixedRealityController>();
+            }
+            return Enumerable.Empty<IMixedRealityController>().ToArray();
+        }
+
+        /// <inheritdoc />
+        public bool CheckCapability(MixedRealityCapability capability)
+        {
+            return (capability == MixedRealityCapability.ArticulatedHand);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (OVRPlugin.GetHandTrackingEnabled())
+            {
+                RemoveAllControllerDevices();
+                UpdateHands();
+            }
+            else
+            {
+                RemoveAllHandDevices();
+                UpdateControllers();
+            }
+        }
+
+        #region Controller Management
+        protected void UpdateControllers()
+        {
+            UpdateController(OVRInput.Controller.LTouch, Handedness.Left);
+            UpdateController(OVRInput.Controller.RTouch, Handedness.Right);
+        }
+
+        protected void UpdateController(OVRInput.Controller controller, Handedness handedness)
+        {
+            if (OVRInput.IsControllerConnected(controller) && OVRInput.GetControllerPositionTracked(controller))
+            {
+                var touchController = GetOrAddController(handedness);
+                touchController.UpdateController(controller, cameraRig.trackingSpace);
+            }
+            else
+            {
+                RemoveControllerDevice(handedness);
+            }
+        }
+
+        private OculusQuestController GetOrAddController(Handedness handedness)
+        {
+            if (trackedControllers.ContainsKey(handedness))
+            {
+                return trackedControllers[handedness];
+            }
+
+            var pointers = RequestPointers(SupportedControllerType.ArticulatedHand, handedness);
+            var inputSourceType = InputSourceType.Hand;
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+            var inputSource = inputSystem?.RequestNewGenericInputSource($"Oculus Quest {handedness} Controller", pointers, inputSourceType);
+
+            var controller = new OculusQuestController(TrackingState.Tracked, handedness, inputSource);
+            controller.SetupConfiguration(typeof(OculusQuestController));
+
+            for (int i = 0; i < controller.InputSource?.Pointers?.Length; i++)
+            {
+                controller.InputSource.Pointers[i].Controller = controller;
+            }
+
+            inputSystem?.RaiseSourceDetected(controller.InputSource, controller);
+
+            trackedControllers.Add(handedness, controller);
+
+            return controller;
+        }
+
+        private void RemoveControllerDevice(Handedness handedness)
+        {
+            if (trackedControllers.TryGetValue(handedness, out OculusQuestController controller))
+            {
+                RemoveControllerDevice(controller);
+            }
+        }
+
+        private void RemoveAllControllerDevices()
+        {
+            if (trackedControllers.Count == 0) return;
+
+            // Create a new list to avoid causing an error removing items from a list currently being iterated on.
+            foreach (var controller in new List<OculusQuestController>(trackedControllers.Values))
+            {
+                RemoveControllerDevice(controller);
+            }
+            trackedControllers.Clear();
+        }
+
+        private void RemoveControllerDevice(OculusQuestController controller)
+        {
+            if (controller == null) return;
+            CoreServices.InputSystem?.RaiseSourceLost(controller.InputSource, controller);
+            trackedControllers.Remove(controller.ControllerHandedness);
+
+            // Recycle pointers makes this loop obsolete. If you are using an MRTK version older than 2.3, please use the loop and comment out RecyclePointers.
+            /*
+            foreach (IMixedRealityPointer pointer in controller.InputSource.Pointers)
+            {
+                if (pointer == null) continue;
+                pointer.Controller = null;
+            }
+            */
+            RecyclePointers(controller.InputSource);
+        }
+        #endregion
+
+        #region Hand Management
+        protected void UpdateHands()
+        {
+            UpdateHand(rightHand, rightSkeleton, Handedness.Right);
+            UpdateHand(leftHand, leftSkeleton, Handedness.Left);
+        }
+
+        protected void UpdateHand(OVRHand ovrHand, OVRSkeleton ovrSkeleton, Handedness handedness)
+        {
+            if (ovrHand.IsTracked)
+            {
+                var hand = GetOrAddHand(handedness);
+                hand.UpdateController(ovrHand, ovrSkeleton, cameraRig.trackingSpace);
+            }
+            else
+            {
+                RemoveHandDevice(handedness);
+            }
+        }
+
+        private OculusQuestHand GetOrAddHand(Handedness handedness)
+        {
+            if (trackedHands.ContainsKey(handedness))
+            {
+                return trackedHands[handedness];
+            }
+
+            // Add new hand
+            var pointers = RequestPointers(SupportedControllerType.ArticulatedHand, handedness);
+            var inputSourceType = InputSourceType.Hand;
+
+            IMixedRealityInputSystem inputSystem = Service as IMixedRealityInputSystem;
+            var inputSource = inputSystem?.RequestNewGenericInputSource($"Oculus Quest {handedness} Hand", pointers, inputSourceType);
+
+            var controller = new OculusQuestHand(TrackingState.Tracked, handedness, inputSource);
+            controller.SetupConfiguration(typeof(OculusQuestHand));
+
+            for (int i = 0; i < controller.InputSource?.Pointers?.Length; i++)
+            {
+                controller.InputSource.Pointers[i].Controller = controller;
+            }
+
+            inputSystem?.RaiseSourceDetected(controller.InputSource, controller);
+
+            trackedHands.Add(handedness, controller);
+
+            return controller;
+        }
+
+        private void RemoveHandDevice(Handedness handedness)
+        {
+            if (trackedHands.TryGetValue(handedness, out OculusQuestHand hand))
+            {
+                RemoveHandDevice(hand);
+            }
+        }
+
+        private void RemoveAllHandDevices()
+        {
+            if(trackedHands.Count == 0) return;
+
+            // Create a new list to avoid causing an error removing items from a list currently being iterated on.
+            foreach (var hand in new List<OculusQuestHand>(trackedHands.Values))
+            {
+                RemoveHandDevice(hand);
+            }
+            trackedHands.Clear();
+        }
+
+        private void RemoveHandDevice(OculusQuestHand hand)
+        {
+            if (hand == null) return;
+            CoreServices.InputSystem?.RaiseSourceLost(hand.InputSource, hand);
+            trackedHands.Remove(hand.ControllerHandedness);
+
+            // Recycle pointers makes this loop obsolete. If you are using an MRTK version older than 2.3, please use the loop and comment out RecyclePointers.
+            /*
+            foreach (IMixedRealityPointer pointer in hand.InputSource.Pointers)
+            {
+                if (pointer == null) continue;
+                pointer.Controller = null;
+            }
+            */
+            RecyclePointers(hand.InputSource);
+        }
+        #endregion
+    }
+}
